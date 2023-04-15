6883939a214f23c972073f829c391582b6ab9ccf
Евгения Лашко <smevlk2014@gmail.com>
2023-04-15 18:57:14 +0300

    add drawEmployees, done hw

--- Task/js/app.js
+++ Task/js/app.js
@@ -5,21 +5,17 @@
 import { getBackList } from "./Components/BlackList/BlackList.js";
 import { convertEmployees } from "./Components/ConvertEmployees/ConvertEmployees.js";
 import { data } from "./Components/Data/Data.js";
+import { drawEmployees } from "./Components/DrawEmployees/DrawEmployees.js";
 import { markingForDismissal } from "./Components/MarkingForDismissal/MarkingForDismissal.js";
 
 const app = () => {
   const convertedEmployees = convertEmployees(data);
 
-  console.log("convertedEmployees", convertedEmployees);
-
   const { backList, surnames } = getBackList(convertedEmployees);
-
-  console.log("blacklist", backList);
-  console.log("surnames", surnames);
 
   markingForDismissal(convertedEmployees, backList);
 
-  console.log("after marking for dismissal", convertedEmployees);
+  drawEmployees(convertedEmployees, surnames);
 };
 
 app();
