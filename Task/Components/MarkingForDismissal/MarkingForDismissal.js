ef3472d234ad5aabee290e1c83260ab39691b9c5
Евгения Лашко <smevlk2014@gmail.com>
2023-04-15 15:21:09 +0300

    add marking for dismissal

--- Task/Components/MarkingForDismissal/MarkingForDismissal.js
+++ Task/Components/MarkingForDismissal/MarkingForDismissal.js
@@ -1,1 +1,11 @@
-//   - модуль отмечающий людей для увольнения 
+//   - модуль отмечающий людей для увольнения
+
+export const markingForDismissal = (data, blackList) => {
+  data.forEach((employee) => {
+    if (blackList.includes(employee)) {
+      employee.dismiss = true;
+    } else {
+      employee.dismiss = false;
+    }
+  });
+};
--- Task/app.js
+++ Task/app.js
@@ -5,6 +5,7 @@
 import { getBackList } from "./Components/BlackList/BlackList.js";
 import { convertEmployees } from "./Components/ConvertEmployees/ConvertEmployees.js";
 import { data } from "./Components/Data/Data.js";
+import { markingForDismissal } from "./Components/MarkingForDismissal/MarkingForDismissal.js";
 
 const app = () => {
   const convertedEmployees = convertEmployees(data);
@@ -15,6 +16,10 @@ const app = () => {
 
   console.log("blacklist", backList);
   console.log("surnames", surnames);
+
+  markingForDismissal(convertedEmployees, backList);
+
+  console.log("after marking for dismissal", convertedEmployees);
 };
 
 app();
